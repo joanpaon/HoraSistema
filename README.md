45132b31f21893c0fa712c59f7ef52192ced0418
José A. Pacheco Ondoño <joanpaon@gmail.com>
2017-10-27 18:08:12 +0200

    :tada: First version

---
+++ README.md
@@ -0,0 +1,13 @@
+<pre>
+
+Project .....: HoraSistema
+Summary .....: Java 8 | Netbeans 8.2 | Programas - Hora y Fecha #02
+Creation ....: 26/10/2016
+Modification : 
+Version .....: 
+Author ......: José A. Pacheco Ondoño - joanpaon@gmail.com
+Platform ....: Java 8
+IDE .........: NetBeans 8.2
+Remote ......: https://github.com/joanpaon/HoraSistema.git
+
+</pre>
---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright 2016 José A. Pacheco Ondoño - joanpon@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+/**
+ *
+ * @author José A. Pacheco Ondoño - joanpon@gmail.com
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variables
+        int h;  // Horas
+        int m;  // Minutos
+        int s;  // Segundos
+        
+        // Objeto GregorianCalendar
+        GregorianCalendar gc = new GregorianCalendar();
+        
+        // Obtener valores
+        h = gc.get(Calendar.HOUR_OF_DAY);
+        m = gc.get(Calendar.MINUTE);
+        s = gc.get(Calendar.SECOND);
+        
+        // Mensaje
+        System.out.printf("Hora del sistema ....: %02d:%02d:%02d\n", h, m, s);
+    }
+}
